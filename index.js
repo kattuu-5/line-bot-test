7538c047b85c74cfda0f8422f20a1717f75ff1d8
kattuu-5 <falcom7kbot@gmail.com>
2025-07-26 23:45:22 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const line = require('@line/bot-sdk');
 require('dotenv').config();
 
 const config = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
 const client = new line.Client(config);
